3d878c41e026bf50c07c0e9c0b13ba8544e601c4
JPraskala <praskalaj@gmail.com>
2026-02-21 12:51:00 -0500

    Started Reducer Class

--- src/main/com/example/DocumentSimilarityReducer.java
+++ src/main/com/example/DocumentSimilarityReducer.java
@@ -1,6 +1,11 @@
 package com.example;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import java.io.IOException;
 
-public class DocumentSimilarityReducer {
-    
+public class DocumentSimilarityReducer extends Reducer<Text, Text, Text, IntWritable> {
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+    }
 }
