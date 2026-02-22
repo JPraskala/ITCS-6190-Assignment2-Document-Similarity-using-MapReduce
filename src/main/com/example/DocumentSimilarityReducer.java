06ee964f7d209d39fb01092802824299636403d9
Jacob Praskala <praskalaj@gmail.com>
2026-02-21 19:57:18 -0500

    Modified Reducer

--- src/main/com/example/DocumentSimilarityReducer.java
+++ src/main/com/example/DocumentSimilarityReducer.java
@@ -1,11 +1,19 @@
 package com.example;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-public class DocumentSimilarityReducer extends Reducer<Text, Text, Text, IntWritable> {
+public class DocumentSimilarityReducer extends Reducer<Text, Text, Text, DoubleWritable> {
+    Set<String> aDocumentWords = new HashSet<>();
+    Set<String> bDocumentWords = new Hashset<>();
+
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        // Calculate Jaccard Similarity (A intersection B) / (A Union B)
+
         
+
     }
 }
